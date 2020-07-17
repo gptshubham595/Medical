5bfaeb010985dad681e8a9c3a7a46dd491829393
gptshubham595 <gptshubham595@gmail.com>
2020-07-18 03:01:27 +0530

    Update UI DESIGN

--- app/src/main/java/com/iitg/reportscanner/Login.java
+++ app/src/main/java/com/iitg/reportscanner/Login.java
@@ -9,15 +9,10 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -54,38 +49,28 @@ public class Login extends AppCompatActivity {
         password = findViewById(R.id.password);
 
         login = findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TextUtils.isEmpty(email.getText().toString())) email.setError("Enter Email");
-                if (TextUtils.isEmpty(password.getText().toString()))
-                    password.setError("Enter Password");
-                if (password.getText().toString().length() < 6) password.setError("Min 6 digits");
-                if (!TextUtils.isEmpty(email.getText().toString()) && !TextUtils.isEmpty(password.getText().toString()) && password.getText().toString().length() >= 6) {
-                    loginProgressBar.setVisibility(View.VISIBLE);
-                    loginUser(email.getText().toString(), password.getText().toString());
-                }
+        login.setOnClickListener(view -> {
+            if (TextUtils.isEmpty(email.getText().toString())) email.setError("Enter Email");
+            if (TextUtils.isEmpty(password.getText().toString()))
+                password.setError("Enter Password");
+            if (password.getText().toString().length() < 6) password.setError("Min 6 digits");
+            if (!TextUtils.isEmpty(email.getText().toString()) && !TextUtils.isEmpty(password.getText().toString()) && password.getText().toString().length() >= 6) {
+                loginProgressBar.setVisibility(View.VISIBLE);
+                loginUser(email.getText().toString(), password.getText().toString());
             }
         });
 
-        findViewById(R.id.register).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(getApplicationContext(), Register.class);
-                        startActivity(i);
-                        CustomIntent.customType(Login.this,"fadein-to-fadeout");
-                    }
+        findViewById(R.id.register).setOnClickListener(v -> {
+                    Intent i = new Intent(getApplicationContext(), Register.class);
+                    startActivity(i);
+                    CustomIntent.customType(Login.this, "fadein-to-fadeout");
                 }
         );
-        findViewById(R.id.forgot).setOnClickListener(new View.OnClickListener() {
-                                                         @Override
-                                                         public void onClick(View v) {
-                                                             Intent i = new Intent(getApplicationContext(), Forgot.class);
-                                                             startActivity(i);
-                                                             CustomIntent.customType(Login.this, "fadein-to-fadeout");
-                                                         }
-                                                     }
+        findViewById(R.id.forgot).setOnClickListener(v -> {
+                    Intent i = new Intent(getApplicationContext(), Forgot.class);
+                    startActivity(i);
+                    CustomIntent.customType(Login.this, "fadein-to-fadeout");
+                }
         );
     }
 
@@ -93,46 +78,40 @@ public class Login extends AppCompatActivity {
     private void loginUser(String email, String password) {
         //   Toast.makeText(this, email, Toast.LENGTH_SHORT).show();
         try {
-            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
 
-                    if (task.isSuccessful()) {
+                if (task.isSuccessful()) {
 
-                        mLoginProgress.dismiss();
+                    mLoginProgress.dismiss();
 
-                        String current_user_id = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
-                        String deviceToken = FirebaseInstanceId.getInstance().getToken();
+                    String current_user_id = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+                    String deviceToken = FirebaseInstanceId.getInstance().getToken();
 
-                        mUserDatabase.child(current_user_id).child("device_token").setValue(deviceToken).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                if (mAuth.getCurrentUser().isEmailVerified()) {
-                                    Intent mainIntent = new Intent(getApplicationContext(), MainActivity2.class);
-                                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                    startActivity(mainIntent);
-                                    CustomIntent.customType(Login.this,"fadein-to-fadeout");
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Please Verify Email or enter correct details", Toast.LENGTH_SHORT).show();
-                                }
-                                finish();
-                                loginProgressBar.setVisibility(View.INVISIBLE);
-
-                            }
-                        });
-
-
-                    } else {
+                    mUserDatabase.child(current_user_id).child("device_token").setValue(deviceToken).addOnSuccessListener(aVoid -> {
+                        if (mAuth.getCurrentUser().isEmailVerified()) {
+                            Intent mainIntent = new Intent(getApplicationContext(), MainActivity2.class);
+                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(mainIntent);
+                            CustomIntent.customType(Login.this, "fadein-to-fadeout");
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Please Verify Email or enter correct details", Toast.LENGTH_SHORT).show();
+                        }
+                        finish();
                         loginProgressBar.setVisibility(View.INVISIBLE);
-                        mLoginProgress.hide();
 
-                        String task_result = Objects.requireNonNull(task.getException()).getMessage();
+                    });
 
-                        Toast.makeText(getApplicationContext(), "Error : " + task_result, Toast.LENGTH_LONG).show();
 
-                    }
+                } else {
+                    loginProgressBar.setVisibility(View.INVISIBLE);
+                    mLoginProgress.hide();
+
+                    String task_result = Objects.requireNonNull(task.getException()).getMessage();
+
+                    Toast.makeText(getApplicationContext(), "Error : " + task_result, Toast.LENGTH_LONG).show();
 
                 }
+
             });
         } catch (Exception e) {
             e.printStackTrace();
@@ -156,6 +135,6 @@ public class Login extends AppCompatActivity {
         Intent main = new Intent(Login.this, MainActivity2.class);
         main.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(main);
-        CustomIntent.customType(Login.this,"fadein-to-fadeout");
+        CustomIntent.customType(Login.this, "fadein-to-fadeout");
     }
 }
--- app/src/main/java/com/iitg/reportscanner/Register.java
+++ app/src/main/java/com/iitg/reportscanner/Register.java
@@ -4,19 +4,16 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -24,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 import maes.tech.intentanim.CustomIntent;
 
@@ -31,8 +29,8 @@ public class Register extends AppCompatActivity {
     private ProgressDialog mLoginProgress;
 
     private FirebaseAuth mAuth;
-
-    private DatabaseReference mUserDatabase, mDatabase;
+    public static String[] device_token = {null};
+    private DatabaseReference mDatabase;
     AppCompatButton register;
     EditText email, pswd, name, age, cnfpswd;
     private ProgressBar loginProgressBar;
@@ -44,7 +42,7 @@ public class Register extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
         mLoginProgress = new ProgressDialog(this, R.style.dialog);
-        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        DatabaseReference mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
         loginProgressBar = findViewById(R.id.progressLogin);
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
@@ -53,42 +51,39 @@ public class Register extends AppCompatActivity {
         cnfpswd = findViewById(R.id.confirmpassword);
 
         register = findViewById(R.id.register);
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(email.getText().toString())) {
-                    Toast.makeText(Register.this, "Enter mail id", Toast.LENGTH_SHORT).show();
-                    email.setError("Enter email");
-                } else if (TextUtils.isEmpty(name.getText().toString())) {
-                    Toast.makeText(Register.this, "Enter name", Toast.LENGTH_SHORT).show();
-                    name.setError("Enter name!!");
-                } else if (TextUtils.isEmpty(age.getText().toString())) {
-                    Toast.makeText(Register.this, "Enter age", Toast.LENGTH_SHORT).show();
-                    age.setError("Enter age");
-                } else if (pswd.getText().toString().length() < 6) {
-                    Toast.makeText(Register.this, "Min 6 digit", Toast.LENGTH_SHORT).show();
-                    pswd.setError("Min 6 digit");
-                } else if (TextUtils.isEmpty(pswd.getText().toString())) {
-                    Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT).show();
-                    pswd.setError("Enter password");
-                } else if (TextUtils.isEmpty(cnfpswd.getText().toString())) {
-                    Toast.makeText(Register.this, "Enter confirmation password", Toast.LENGTH_SHORT).show();
-                    cnfpswd.setError("Enter Confirmation Password");
-                } else if (!pswd.getText().toString().equals(cnfpswd.getText().toString())) {
-                    cnfpswd.setError("Password not matched!!");
-                    Toast.makeText(Register.this, "Password not matched", Toast.LENGTH_SHORT).show();
+        register.setOnClickListener(v -> {
+            if (TextUtils.isEmpty(email.getText().toString())) {
+                Toast.makeText(Register.this, "Enter mail id", Toast.LENGTH_SHORT).show();
+                email.setError("Enter email");
+            } else if (TextUtils.isEmpty(name.getText().toString())) {
+                Toast.makeText(Register.this, "Enter name", Toast.LENGTH_SHORT).show();
+                name.setError("Enter name!!");
+            } else if (TextUtils.isEmpty(age.getText().toString())) {
+                Toast.makeText(Register.this, "Enter age", Toast.LENGTH_SHORT).show();
+                age.setError("Enter age");
+            } else if (pswd.getText().toString().length() < 6) {
+                Toast.makeText(Register.this, "Min 6 digit", Toast.LENGTH_SHORT).show();
+                pswd.setError("Min 6 digit");
+            } else if (TextUtils.isEmpty(pswd.getText().toString())) {
+                Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT).show();
+                pswd.setError("Enter password");
+            } else if (TextUtils.isEmpty(cnfpswd.getText().toString())) {
+                Toast.makeText(Register.this, "Enter confirmation password", Toast.LENGTH_SHORT).show();
+                cnfpswd.setError("Enter Confirmation Password");
+            } else if (!pswd.getText().toString().equals(cnfpswd.getText().toString())) {
+                cnfpswd.setError("Password not matched!!");
+                Toast.makeText(Register.this, "Password not matched", Toast.LENGTH_SHORT).show();
 
-                } else {
-                    loginProgressBar.setVisibility(View.VISIBLE);
-                    register_user(email.getText().toString(), pswd.getText().toString(), name.getText().toString(), age.getText().toString());
-                    cnfpswd.setError(null);
-                    pswd.setError(null);
-                    email.setError(null);
-                    age.setError(null);
-                    name.setError(null);
+            } else {
+                loginProgressBar.setVisibility(View.VISIBLE);
+                register_user(email.getText().toString(), pswd.getText().toString(), name.getText().toString(), age.getText().toString());
+                cnfpswd.setError(null);
+                pswd.setError(null);
+                email.setError(null);
+                age.setError(null);
+                name.setError(null);
 
 
-                }
             }
         });
 
@@ -97,59 +92,64 @@ public class Register extends AppCompatActivity {
 
     private void register_user(String email, String password, final String name, final String age) {
 
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
 
-                if (task.isSuccessful()) {
+            if (task.isSuccessful()) {
 
 
-                    FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
-                    String uid = current_user.getUid();
+                FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = Objects.requireNonNull(current_user).getUid();
 
-                    mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+                mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
 
-                    String device_token = FirebaseInstanceId.getInstance().getToken();
 
-                    HashMap<String, String> userMap = new HashMap<>();
-                    userMap.put("name", name);
-                    userMap.put("status", "Hi there I'm using iReport App.");
-                    userMap.put("image", "default");
-                    userMap.put("age", "" + age);
-                    userMap.put("address", "default");
-                    userMap.put("mobile", "default");
-                    userMap.put("thumb_image", "default");
-                    userMap.put("device_token", device_token);
-
-                    mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-
-                            if (task.isSuccessful()) {
-
-                                mLoginProgress.dismiss();
-                                sendEmailVerification();
-                                loginProgressBar.setVisibility(View.INVISIBLE);
-                                Toast.makeText(getApplicationContext(), "Registered!! Please verify Email Confirmation and Login", Toast.LENGTH_LONG).show();
-                                Intent i = new Intent(getApplicationContext(), Login.class);
-                                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                startActivity(i);
-                                CustomIntent.customType(Register.this, "fadein-to-fadeout");
-
+                FirebaseInstanceId.getInstance().getInstanceId()
+                        .addOnCompleteListener(task12 -> {
+                            if (!task12.isSuccessful()) {
+                                Log.w("TAG", "getInstanceId failed", task12.getException());
+                                return;
                             }
 
-                        }
-                    });
+                            // Get new Instance ID token
+                            device_token[0] = Objects.requireNonNull(task12.getResult()).getToken();
 
 
-                } else {
+                        });
+                HashMap<String, String> userMap = new HashMap<>();
+                userMap.put("name", name);
+                userMap.put("status", "Hi there I'm using iReport App.");
+                userMap.put("image", "default");
+                userMap.put("age", "" + age);
+                userMap.put("address", "default");
+                userMap.put("mobile", "default");
+                userMap.put("thumb_image", "default");
+                userMap.put("device_token", device_token[0]);
 
-                    mLoginProgress.hide();
-                    Toast.makeText(getApplicationContext(), "Cannot Sign in. Please check the form and try again.", Toast.LENGTH_LONG).show();
-                    loginProgressBar.setVisibility(View.INVISIBLE);
-                }
+                mDatabase.setValue(userMap).addOnCompleteListener(task1 -> {
 
+                    if (task1.isSuccessful()) {
+
+                        mLoginProgress.dismiss();
+                        sendEmailVerification();
+                        loginProgressBar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(getApplicationContext(), "Registered!! Please verify Email Confirmation and Login", Toast.LENGTH_LONG).show();
+                        Intent i = new Intent(getApplicationContext(), Login.class);
+                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(i);
+                        CustomIntent.customType(Register.this, "fadein-to-fadeout");
+
+                    }
+
+                });
+
+
+            } else {
+
+                mLoginProgress.hide();
+                Toast.makeText(getApplicationContext(), "Cannot Sign in. Please check the form and try again.", Toast.LENGTH_LONG).show();
+                loginProgressBar.setVisibility(View.INVISIBLE);
             }
+
         });
 
     }
@@ -158,15 +158,12 @@ public class Register extends AppCompatActivity {
     private void sendEmailVerification() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(getApplicationContext(), "Check your Email", Toast.LENGTH_LONG).show();
-                        FirebaseAuth.getInstance().signOut();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "ERROR OCCURED TRY AGAIN!!", Toast.LENGTH_LONG).show();
-                    }
+            user.sendEmailVerification().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Check your Email", Toast.LENGTH_LONG).show();
+                    FirebaseAuth.getInstance().signOut();
+                } else {
+                    Toast.makeText(getApplicationContext(), "ERROR OCCURED TRY AGAIN!!", Toast.LENGTH_LONG).show();
                 }
             });
         }
